84db9209bc79385bb5dc27452997a091793f6317
SHAMOVMAKAR <shamovmakar@mail.ru>
2025-02-13 15:55:43 +0500

    Добавлена регистрация пользователей

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Сервер работает!"
+# Настройки базы данных
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # Секретный ключ для токенов
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+# Модель пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+# Создаем базу данных
+with app.app_context():
+    db.create_all()
+
+# Регистрация пользователя
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({'message': 'Пользователь зарегистрирован!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
